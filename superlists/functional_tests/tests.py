09b602a1b792905959469a8ef929bc56af06b714
Janusz Kamieński <janusz.kamienski@hummingbird.pl>
2017-04-06 14:40:33 +0200

    functional tests now in django

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,14 +1,18 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.live_server_url)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -37,6 +41,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         self.fail('Finish the test!')
-
-if __name__ == '__main__':
-    unittest.main(warnings = 'ignore')
